697e02e15bc2b4ad2f9afffcba3e701f93ca803c
accforgit <kqnight@qq.com>
2020-12-09 15:18:16 +0800

    add img

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 
-const pre = document.querySelector('pre')
-if (!pre) return
-document.body.innerHTML= "<div id='wrapper'>" + marked(pre.textContent, {
-  breaks: true,
-  highlight: function(code) {
-    return hljs.highlightAuto(code).value;
-  }
-}) + "</div>";
-document.body.style.display = 'block';
+function run() {
+  const pre = document.querySelector('pre')
+  if (!pre) return
+  document.body.innerHTML= "<div id='wrapper'>" + marked(pre.textContent, {
+    breaks: true,
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value;
+    }
+  }) + "</div>";
+  document.body.style.display = 'block';
+}
+run()
