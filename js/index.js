f1f0624f17033704335f0bf0732b49401c09996a
accforgit <kqnight@qq.com>
2020-12-12 22:43:37 +0800

    add imgManager

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 
 function run() {
+  // 给使用 // 开头的 <img /> 添加 https 协议
+  // //pic.com/1.png => https://pic.com/1.png
+  function imgManager(mdStr) {
+    return mdStr.replace(/(?<=<img[\s\S]+?src=")\/\/.+?(?="[\s\S]*>)/g, mt => {
+      return 'https:' + mt
+    })
+  }
   const pre = document.querySelector('pre')
   if (!pre) return
-  document.body.innerHTML= "<div id='wrapper'>" + marked(pre.textContent, {
+  document.body.innerHTML= "<div id='wrapper'>" + imgManager(marked(pre.textContent, {
     breaks: true,
     highlight: function(code) {
       return hljs.highlightAuto(code).value;
-    }
-  }) + "</div>";
+    },
+  })) + "</div>";
   document.body.style.display = 'block';
 }
 run()
